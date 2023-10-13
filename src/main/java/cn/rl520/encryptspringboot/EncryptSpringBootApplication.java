dadfaa2fa97bfd0d38c7dc64c7b1ab834bc6827a
王文博 <wwb@echase.cn>
2023-10-13 15:32:13 +0800

    第一次提交github

---
+++ src/main/java/cn/rl520/encryptspringboot/EncryptSpringBootApplication.java
@@ -0,0 +1,13 @@
+package cn.rl520.encryptspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/rl520/encryptspringboot/EncryptSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.rl520.encryptspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
